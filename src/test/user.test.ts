b516d2bae80909fc9b51de0317e5565183adc914
3rooks <Brookslynx@gmail.com>
2023-02-28 21:32:45 -0300

    ⚡️ backend pdf

--- src/test/user.test.ts
+++ src/test/user.test.ts
@@ -1,6 +1,21 @@
+import { faker } from '@faker-js/faker';
 import test from 'ava';
 import got from 'got-cjs';
 import { expectStatusCode } from './utils/status';
+
+faker.locale = 'en_US';
+
+export const USER_A = {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+};
+
+export const USER_B = {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+};
 
 test.before(() => {
     console.log(`
